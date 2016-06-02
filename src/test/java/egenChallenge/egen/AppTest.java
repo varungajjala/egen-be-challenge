13200b1c261728d3fb94613c6110b88d3678d377
Varun Kumar Reddy Gajjala <Varun@Varuns-MacBook-Pro.local>
2016-06-02 12:20:04 -0500

    rest client test cases

--- src/test/java/egenChallenge/egen/AppTest.java
+++ src/test/java/egenChallenge/egen/AppTest.java
@@ -1,38 +1,111 @@
+/**
+ * 
+ */
 package egenChallenge.egen;
 
-import junit.framework.Test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import User.UserService;
+import junit.framework.Assert;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import testdata.testData;
 
 /**
- * Unit test for simple App.
+ * @author Varun
+ *
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
+public class AppTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		new App("testuser");
+		Thread.sleep(2000);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		UserService.getCollection("testuser").drop();
+	}
+
+	/**
+	 * Test method for {@link egenChallenge.egen.App#main(java.lang.String[])}.
+	 */
+	public void testMain() {
+
+        String response = this.execUrl("/createUser", testData.JSON_LONG, "PUT");
+        Assert.assertEquals("User created", "{\"message\":\"User created successfully\"}", response);
+
+		String response1 = this.execUrl("/getAllUsers", null, "GET");
+        Assert.assertEquals("1 user should match", "["+testData.JSON_LONG+"]", response1.replace("\"", "'"));
+
+        
+        String response2 = this.execUrl("/updateUser", testData.JSON_LONG_UPDATED, "PUT");
+        Assert.assertEquals("1 user should be updated", "{\"message\":\"Updated Successfully\"}", response2);
+
+	}
+	
+	/**
+     * Rest client connection
      *
-     * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    private String execUrl(final String testUrl, final String json, final String methodType) {
+    	StringBuffer sb = new StringBuffer();
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    	try {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    		URL url = new URL("http://localhost:4567"+testUrl);
+    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    		conn.setDoOutput(true);
+    		conn.setRequestMethod(methodType);
+    		conn.setRequestProperty("Content-Type", "application/json");
+
+    		if(json != null){
+	    		OutputStream os = conn.getOutputStream();
+	    		os.write(json.getBytes());
+	    		os.flush();
+    		}
+
+    		/*if (conn.getResponseCode() != 200) {
+    			throw new RuntimeException("Failed : HTTP error code : "
+    				+ conn.getResponseCode());
+    		}*/
+
+    		BufferedReader br = new BufferedReader(new InputStreamReader(
+    				(conn.getInputStream())));
+
+    		String output;
+    		
+    		System.out.println("Output from Server .... \n");
+    		while ((output = br.readLine()) != null) {
+    			sb.append(output);
+    		}
+
+    		conn.disconnect();
+
+    	  } catch (MalformedURLException e) {
+
+    		e.printStackTrace();
+
+    	  } catch (IOException e) {
+
+    		e.printStackTrace();
+
+    	 }
+    	
+    	return sb.toString();
     }
+    
+
 }
